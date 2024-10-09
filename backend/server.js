7f6e535bc4aa51006d6fa8bf6737ac89e72ca7df
vortix2950 <vortix2950@ksu.edu>
2024-10-09 15:14:39 +0300

    MongoDB demo

--- backend/server.js
+++ backend/server.js
@@ -1,85 +1,88 @@
 import express from "express"
+import mongoose from "mongoose"
+import Article from "./models/Article.js"
+import Book from "./models/Book.js"
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+
+startConnection().catch(err => console.log(err))
+async function startConnection() {
+    // console.log()
+    await mongoose.connect(process.env.CONNECTION_STRING)
+
+    console.log("connected to Mongoose")
+}
+
+
 const app = express()
 app.use(express.json())
 
 let port = 8080
-let blogs =[]
-let users =[]
+
 app.listen(port || 7000, () => console.log(`listening to ${port || 7000}`))
 
-app.get("/blogs",(req,res)=>{
-    res.json(blogs)
-})
 
-app.get("/blogs/:id",(req,res)=>{
-    let id = parseFloat( req.params.id)
-    let filtered = blogs.filter(post => post.id===id)
-    res.json(filtered)
-})
-
-app.post("/blogs",(req,res)=>{
-    let {username,post} = req.body
-    blogs.push({username,post,"id":blogs.length+1})
-
-    res.json(blogs)
-})
-
-app.delete("/blogs/:id",(req,res)=>{
-    let id = parseInt(req.params.id)
-    let filtered = blogs.filter(post => post.id!==id)
-    blogs = filtered
-    res.status(200).json({"mess":"post deleted"})
+app.get("/book/:id", (req, res) => {
+    let {id} = req.params
+ 
+    Book.findOne({"_id":id})
+    .then(result=>res.json(result))
 
 })
 
-
-app.patch("/blogs/:id",(req,res)=>{
-    
-    let index;
-    for (let i =0; i < blogs.length; i++){
-
-        if (parseInt(blogs[i].id)===parseInt(req.params.id))  index=i
+app.post("/book", (req, res) => {
+    let { author, title, version, publishedDate, isDigital, price, languages, catagory } = req.body
+    let book = new Book({
+        author: author || "",
+        title: title || "",
+        version: version || "",
+        publishedDate: publishedDate || "",
+        isDigital: isDigital || "",
+        price: price || "",
+        languages: languages || "",
+        catagory: catagory || ""
     }
+    )
+    book.save()
+    .then(result=>res.json(result))
 
-    let body =req.body
-    let {username,post} = body
-    if(!index) return res.send("not found")
-    blogs[index]= {"username":(username|| blogs[index].username) , "post": (post|| blogs[index].post),"id":blogs[index].id}
-   
-    res.json(blogs[index])
-})
-
-
-app.post("/signup",(req,res)=>{
-    let {username,password,email} = req.body
-
-    if(!username) return res.json({"mess":"Username is empty"})
-    if(!password) return res.json({"mess":"password is empty"})
-
-    if(users.filter(user=>user.username===username).length <=0){
-        let new_user = {username,password,email,"id":users.length+1}
-
-        users.push(new_user)
-        return res.json(new_user)
-    }
-    return res.json({"mess":"User already exists"})
 })
 
 
 
-app.post("/login",(req,res)=>{
+app.get("/book", (req, res) => {
+  Book.find()
+    .then(result=>res.json(result))
 
-    let {username,password} = req.body
+})
 
-    if(!username) return res.json({"mess":"Username is empty"})
-    if(!password) return res.json({"mess":"password is empty"})
-    let user_filter = users.filter(user=>user.username===username)
-    if(user_filter){
-        
-       if(user_filter[0].username===username && user_filter[0].password === password){
-        res.json({"mess":"login sucessful"})
-       }
+
+
+
+
+
+app.patch("/book/:id", (req, res) => {
+    const userId = req.params.id
+    const { author, title, version, publishedDate, isDigital, price, languages, catagory } = req.body
+    console.log(author)
+    console.log(userId)
+    const book = new Book({
+        author: author 
     }
-    return res.json({"mess":"Incorrect login"})
+    )
+   // findByIdAndUpdate(id,{author:req.body.author},{new:true})
+    Book.findByIdAndUpdate(userId,book)
+    .then(result=>res.send())
+
+})
+
+
+app.patch("/book/:id", (req, res) => {
+  let {id} =req.params
+   // findByIdAndUpdate(id,{author:req.body.author},{new:true})
+    Book.findByIdAndDelete(id,book)
+    .then(result=>res.send())
 
 })
